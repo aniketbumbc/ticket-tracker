e700d31346650244c0d13d0ded89609176ed4f0d
aniket <aniket.umbc@gmail.com>
2021-05-09 17:41:35 +0530

    tech reducer added

---
+++ src/reducers/techReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  tech: null,
+  loading: false,
+  error: null,
+};
+
+const techReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default techReducer;
