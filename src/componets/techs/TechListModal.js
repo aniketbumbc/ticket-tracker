edef0c0f67ca6f20d4595d049ff18b8fd8e6b7b5
aniket <aniket.umbc@gmail.com>
2021-05-09 18:15:53 +0530

    get tech added

--- src/componets/techs/TechListModal.js
+++ src/componets/techs/TechListModal.js
@@ -1,32 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import TechItem from './TechItem';
-const TechListModal = () => {
-  const [techs, setTechs] = useState([]);
-  const [loading, setLoading] = useState(false);
-
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { getTechs } from '../../actions/techActions';
+const TechListModal = ({ getTechs, tech: { techs, loading } }) => {
   useEffect(() => {
     getTechs();
     //eslint-disable-next-line
   }, []);
 
-  const getTechs = async () => {
-    setLoading(true);
-    const res = await fetch('/techs');
-    const data = await res.json();
-    setTechs(data);
-    setLoading(false);
-  };
   return (
     <div id='tech-list-modal' className='modal'>
       <div className='modal-content'>
         <h4> Technician List</h4>
         <ul className='collection'>
           {!loading &&
+            techs !== null &&
             techs.map((tech) => <TechItem tech={tech} key={tech.id} />)}
         </ul>
       </div>
     </div>
   );
 };
+const mapStaeToProp = (state) => ({
+  tech: state.tech,
+});
 
-export default TechListModal;
+TechListModal.propTypes = {
+  getTechs: PropTypes.func.isRequired,
+};
+
+export default connect(mapStaeToProp, { getTechs })(TechListModal);
--- src/reducers/techReducer.js
+++ src/reducers/techReducer.js
@@ -1,11 +1,36 @@
+import {
+  ADD_TECH,
+  GET_TECHS,
+  SET_LOADING,
+  TECHS_ERROR,
+  DELET_TECH,
+} from '../actions/types.js';
+
 const initialState = {
-  tech: null,
+  techs: null,
   loading: false,
   error: null,
 };
 
-const techReducer = (state, action) => {
+const techReducer = (state = initialState, action) => {
   switch (action.type) {
+    case GET_TECHS:
+      return {
+        ...state,
+        techs: action.payload,
+        loading: false,
+      };
+
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case TECHS_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
     default:
       return state;
   }
